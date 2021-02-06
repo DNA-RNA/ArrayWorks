d4f3391cb2cbfb971598f131c223dddd32fb4c7d
DNA-RNA <77885953+DNA-RNA@users.noreply.github.com>
2021-02-06 14:12:32 +0300

    ArrayWorks

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //Collections,list kullanmadan diziye bir eleman ekleme
+            string[] isimler1 = new string[] { "Rana", "Meryem", "Asli", "Zehra" };
+            string[] temp = new string[] { "Rana", "Meryem", "Asli", "Zehra" };
+            //Esas dizimiz olan isimleri gecici olarak atayacagimiz bir temp dizisi olusturdum.
+            
+
+            string[] birlesim = new string[isimler1.Length + temp.Length];
+            //Sonradan ekleyecegimiz elemanlarla basta tanimladigimiz diziyi birlestirecek dizi.
+           
+            isimler1 = new string[6];
+            //Yeni elemanlar ekleyecegim icin diziyi newledim. 
+            isimler1[4] = "Munire";
+            isimler1[5] = "Fatma";
+          
+            for (int i = 0; i < isimler1.Length; i++)
+            {
+                birlesim[i] = isimler1[i];
+            }
+            for (int i = 0; i < temp.Length; i++)
+            {
+                birlesim[i] = temp[i];
+            }
+            foreach (var dizi in birlesim)
+            {
+                Console.WriteLine(dizi);
+            }
+
+
+
+
+
+
+
+        }
+        
+
+
+    }
+}
